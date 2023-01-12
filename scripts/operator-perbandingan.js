20bc23920c20896962d734a713157e63f53655e7
rudigigih1907 <rudigigih0719@gmail.com>
2023-01-12 14:05:37 +0700

    Operator Perbandingan

---
+++ scripts/operator-perbandingan.js
@@ -0,0 +1,23 @@
+let result = 5 == "5";
+document.writeln("<p>" + result + "</p>");
+
+result = 5 === "5";
+document.writeln("<p>" + result + "</p>");
+
+result = 5 > 2;
+document.writeln("<p>" + result + "</p>");
+
+result = 5 < 2;
+document.writeln("<p>" + result + "</p>");
+
+result = 5 >= 2;
+document.writeln("<p>" + result + "</p>");
+
+result = 5 <= 2;
+document.writeln("<p>" + result + "</p>");
+
+result = 5 != 2;
+document.writeln("<p>" + result + "</p>");
+
+result = 5 !== "5";
+document.writeln("<p>" + result + "</p>");

5d733d1b34ef648adc6a5657ad60ef5b8d9bf987
rudigigih1907 <rudigigih0719@gmail.com>
2023-01-12 13:18:32 +0700

    Operator Unary

---
+++ scripts/operator-unary.js
@@ -0,0 +1,11 @@
+let result = +1;
+document.writeln("<p>" + result + "</p>");
+
+result-- // result = result - 1; result -= 1;
+document.writeln("<p>" + result + "</p>");
+
+result++ // result = result + 1; result += 1;
+document.writeln("<p>" + result + "</p>");
+
+result = -result; // -1
+document.writeln("<p>" + result + "</p>");

47e5484e3f71357930555e5bea764f7a89deac01
rudigigih1907 <rudigigih0719@gmail.com>
2023-01-16 14:21:08 +0700

    If Expression

---
+++ scripts/if-expression.js
@@ -0,0 +1,11 @@
+const examValue = 70;
+
+if (examValue > 80) {
+    document.writeln("<p>Selamat A </p>");
+} else if (examValue > 70) {
+     document.writeln("<p>Selamat B </p>");
+} else if (examValue > 60) {
+    document.writeln("<p>Selamat C </p>");
+} else {
+    document.writeln("<p>Coba Lagi</p>");
+}
